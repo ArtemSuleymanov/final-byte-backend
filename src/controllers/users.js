e91e73f15906932f094f3b481153508385e6d5ac
Olha Vorobiova <vorobjova1609@gmail.com>
2025-05-20 21:12:07 +0300

    user info with swagger

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,6 +3,11 @@ import { getUserById } from '../services/users.js';
 export const getUserController = async (req, res) => {
   const userId = req.user._id;
   const user = await getUserById(userId);
+  if (!user) {
+    return res.status(401).json({
+      message: 'User is unauthorized',
+    });
+  }
 
   res.json({
     status: 200,
